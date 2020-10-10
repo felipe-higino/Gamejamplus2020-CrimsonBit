3f6fcd0719696b6578cce1f9f0718ee9a1312bbc
felipe <felipe.higino.ps@gmail.com>
2020-10-10 13:50:21 -0300

    UI Placeholder
    - Import text mesh pro
    - UI Mannager and UI Hidable (Utils and Project)
    - Main screen
    - Game hud
    - Pause/win/loose screen

---
+++ gamejam-plus-2020-jogo/Assets/ProjectAssets/Scripts/UI/UIManager_GJPlus.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using FelipeUtils.UIMannagement;
+
+public enum ScreenGroups
+{
+    MainScreen,
+    GameHud,
+    PauseScreen,
+    WinScreen,
+    LooseScreen,
+}
+
+public class UIManager_GJPlus : UIMannager
+{
+    private void Start()
+    {
+        HideAll();
+        //shows just the number one screen
+        if (Screens.Length <= 0)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        actualScreen = ScreenGroups.MainScreen;
+        previousScreen = ScreenGroups.MainScreen;
+        Screens[(int)ScreenGroups.MainScreen].Show();
+
+    }
+    private ScreenGroups actualScreen;
+    private ScreenGroups previousScreen;
+    public ScreenGroups PreviousScreen { get => previousScreen; }
+
+    public void ChangeScreen(ScreenGroups newScreen)
+    {
+        if (actualScreen == newScreen)
+            return;
+
+        HideAll();
+        Screens[(int)newScreen].Show();
+
+        previousScreen = actualScreen;
+        actualScreen = newScreen;
+    }
+}
