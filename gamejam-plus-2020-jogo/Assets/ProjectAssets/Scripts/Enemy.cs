da625365e34f41d9e14b00172b7e112170d3d284
Rouem <raphaelfelipe999@gmail.com>
2020-10-10 16:41:43 -0300

    fps basico

---
+++ gamejam-plus-2020-jogo/Assets/ProjectAssets/Scripts/Enemy.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    PlayerController player;
+    public float speed;
+    Rigidbody rb;
+    public Transform head;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        player = GameObject.FindObjectOfType<PlayerController>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if(RangeOfViewInArea(15f,0.85f))
+            RotateToPlayer();
+    }
+
+    void RotateToPlayer(){
+        transform.LookAt(player.transform);
+        Vector3 rot = transform.eulerAngles;
+        rot.x = 0f;
+        rot.z = 0f;
+            transform.eulerAngles = rot;
+        
+        Vector3 move = rb.position + transform.forward *speed *Time.deltaTime;
+        rb.MovePosition(move);
+    }
+
+
+
+    bool RangeOfViewInArea(float distance, float range)
+	{
+		RaycastHit hit;
+		float direction = Vector3.Dot((player.transform.position - transform.position).normalized , transform.forward);
+		if(Vector3.Distance(transform.position,player.transform.position) < distance)
+		if(Physics.Linecast(head.position, player.transform.position, out hit) && direction > range){
+			return hit.transform.Equals(player.transform);
+		}
+		return false;
+	}
+}
---
+++ gamejam-plus-2020-jogo/Assets/ProjectAssets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Rigidbody rb;
+    public float speed;
+    public float horSpeed;
+    public bool isClimbing;
+
+    public void setIsClimbing(bool isClimbing)
+    {
+        this.isClimbing = isClimbing;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        PlayerDirection();
+    }
+    void FixedUpdate()
+    {
+        if(isClimbing){
+            PlayerClimbing();
+        }else{
+            PlayerMovement();
+        }
+    }
+    void PlayerMovement(){
+        Vector3 direction = (transform.forward * speed * Input.GetAxis("Vertical")) +
+        (transform.right * speed * Input.GetAxis("Horizontal"));
+        Vector3 move = rb.position + direction * Time.deltaTime;
+        rb.MovePosition(move);
+    }
+    void PlayerClimbing(){
+        Vector3 direction = (transform.forward * 1.5f * Input.GetAxis("Vertical")) +
+        (transform.right * 1.5f * Input.GetAxis("Horizontal"));
+        if(Input.GetButtonDown("Space")){
+            rb.velocity = transform.forward * -5f + transform.up;
+            isClimbing = false;
+            return;
+        }
+        rb.velocity = transform.up + direction;
+    }
+    void PlayerDirection(){
+        Vector3 rot = transform.eulerAngles;
+        rot.y += Input.GetAxis("Mouse X") * horSpeed * Time.deltaTime;
+        transform.eulerAngles = rot;
+    }
+
+}
