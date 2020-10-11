7f9cd1d79cc93a41b97bd37643cd53bf7d607d39
Rouem <raphaelfelipe999@gmail.com>
2020-10-11 19:58:09 -0300

    new updates

---
+++ gamejam-plus-2020-jogo/Assets/ProjectAssets/Scripts/RoomGenerator/Room.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject[] Walls;
+    [SerializeField]
+    public Transform[] directions;
+    [SerializeField]
+    public GameObject[] Decorations;
+
+    public int openDirection = -1;
+
+
+    public int SortDirection(){
+        //Sort the direction of new room
+        openDirection = (int) Random.Range(0,directions.Length);
+        //If the direction is the position of last room, sort again.
+        CheckSelectedDirection(openDirection);
+        //If not, return the direction.
+        return openDirection;
+    }
+
+    public int SortDirection(int lastDir){
+        //Sort the direction of new room
+        openDirection = (int) Random.Range(0,directions.Length);
+        if(openDirection == lastDir){
+            openDirection++;
+            if(openDirection > directions.Length-1)
+                openDirection = 0;
+        }
+        //If the direction is the position of last room, sort again.
+        CheckSelectedDirection(openDirection);
+        //If not, return the direction.
+        return openDirection;
+    }
+
+    public void CreateDecoration(){
+        if(Decorations.Length < 1)return;
+        int sort = Random.Range(0,Decorations.Length);
+        Instantiate(Decorations[sort], transform.position,Quaternion.identity);
+    }
+
+
+
+    //public int CheckSelectedDirection(int sorted){
+    public void CheckSelectedDirection(int sorted){
+        RaycastHit hit;
+        if(
+            Physics.Linecast(transform.position,directions[openDirection].position,out hit)
+            && hit.transform.GetComponent<Room>() != null
+        ){
+            
+            for(int i = 0; i < directions.Length; i++){
+                if(
+                    Physics.Linecast(transform.position,directions[i].position,out hit)
+                    && hit.transform.GetComponent<Room>() != null
+                )
+                    continue;
+                else{
+                    openDirection = i;
+                    return;
+                }
+            }
+        }
+
+    }
+    
+}
---
+++ gamejam-plus-2020-jogo/Assets/ProjectAssets/Scripts/RoomGenerator/RoomManager.cs
@@ -0,0 +1,133 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+    public int numberOfRooms;
+    public GameObject roomPrefab;
+    public Transform roomStartPosition;
+    public static RoomManager instance;
+    public GameObject player, monster;
+    int roomNumber;
+    List<GameObject> rooms;
+
+    void Awake(){
+        instance = this;
+    }
+
+    void Start(){
+        rooms = new List<GameObject>();
+        GenerateRooms();
+    }
+
+    /* public void GenerateRooms(){
+        Instantiate(roomPrefab,roomStartPosition.position,Quaternion.identity);
+    } */
+    int lastDir = 0;
+    public void GenerateRooms(){
+        //Creation of first room
+        int playerRoom, monsterRoom;
+        playerRoom = (numberOfRooms/4);
+        monsterRoom = ((numberOfRooms/4)*2);
+        print(playerRoom+" "+monsterRoom);
+
+        GameObject newRoom;
+        newRoom = Instantiate(roomPrefab,roomStartPosition.position,Quaternion.identity);
+        newRoom.name = newRoom.name +" "+(roomNumber+1);
+        Room roomScript = newRoom.GetComponent<Room>();
+        rooms.Add(newRoom);
+
+
+        //Creation of some rooms - setting positions
+        for(roomNumber = 1; roomNumber < numberOfRooms; roomNumber++){
+            int dir = roomScript.SortDirection(lastDir);
+            Vector3 nextRoomPosition = roomScript.directions[dir].position;
+            lastDir = dir;
+
+            newRoom = Instantiate(roomPrefab,nextRoomPosition,Quaternion.identity);
+            newRoom.name = newRoom.name +" "+(roomNumber+1);
+            rooms.Add(newRoom);
+            roomScript = newRoom.GetComponent<Room>();
+        }
+        int index = 0;
+        //Creation of walls of rooms, decorations
+        foreach(GameObject currentRoom in rooms){
+            roomScript = currentRoom.GetComponent<Room>();
+            Transform[] direction = roomScript.directions;
+            GameObject[] walls = roomScript.Walls;
+            for(int i = 0; i < walls.Length; i++){
+                if(
+                    Physics.Linecast(currentRoom.transform.position, direction[i].position)
+                ){
+                    continue;
+                }else{
+                    walls[i].SetActive(true);
+                }
+            }
+            roomScript.CreateDecoration();
+
+            if(index == playerRoom){
+                Instantiate(player, currentRoom.transform.position, Quaternion.identity);
+            }
+            if(index == monsterRoom){
+                Instantiate(monster, currentRoom.transform.position, Quaternion.identity);
+            }
+            index++;
+        }
+    }
+
+    public void GenerateSpiralRooms(){
+        //Creation of first room
+        GameObject newRoom;
+        newRoom = Instantiate(roomPrefab,roomStartPosition.position,Quaternion.identity);
+        newRoom.name = newRoom.name +" "+(roomNumber+1);
+        Room roomScript = newRoom.GetComponent<Room>();
+        rooms.Add(newRoom);
+
+        //Creation of some rooms - setting positions
+        for(roomNumber = 1; roomNumber < numberOfRooms; roomNumber++){
+            int dir = roomScript.SortDirection();
+            Vector3 nextRoomPosition = roomScript.directions[dir].position;
+            newRoom = Instantiate(roomPrefab,nextRoomPosition,Quaternion.identity);
+            newRoom.name = newRoom.name +" "+(roomNumber+1);
+            rooms.Add(newRoom);
+            roomScript = newRoom.GetComponent<Room>();
+        }
+
+        //Creation of walls of rooms
+        foreach(GameObject currentRoom in rooms){
+            roomScript = currentRoom.GetComponent<Room>();
+            Transform[] direction = roomScript.directions;
+            GameObject[] walls = roomScript.Walls;
+            for(int i = 0; i < walls.Length; i++){
+                if(
+                    Physics.Linecast(currentRoom.transform.position, direction[i].position)
+                ){
+                    continue;
+                }else{
+                    walls[i].SetActive(true);
+                }
+            }
+        }
+    }
+
+    public void Increase(){
+        roomNumber++;
+    }
+
+    public bool IsFirstRoom
+    {
+        get {
+            return numberOfRooms == 0;
+        }
+    }
+
+     public bool IsLastRoom
+    {
+        get {
+            return numberOfRooms == roomNumber;
+        }
+    }
+
+}
