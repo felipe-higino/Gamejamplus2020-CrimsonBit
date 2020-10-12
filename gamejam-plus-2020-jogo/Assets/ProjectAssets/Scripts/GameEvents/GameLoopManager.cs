42c072e4128d6a97c569ff9a9d3202c76fafb00f
Rouem <raphaelfelipe999@gmail.com>
2020-10-11 21:37:08 -0300

    add bonfire

---
+++ gamejam-plus-2020-jogo/Assets/ProjectAssets/Scripts/GameEvents/GameLoopManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameLoopManager : MonoBehaviour
+{
+    public static GameLoopManager instance;
+    private void Awake() {
+        if(instance!=null)
+            Destroy(this);
+        instance = this;
+    }
+
+
+    public List<Item> itens;
+    public string level;
+    public GameObject goal;
+    public void AddItem(Item item){
+        itens.Add(item);
+    }
+    public void RemoveItem(Item item){
+        itens.Remove(item);
+    }
+    void VictoryCondition(){
+        if(itens.Count < 1){
+            goal.SetActive(true);
+        }
+    }
+
+    public void ReloadScene(){
+        SceneManager.LoadScene(level);
+    }
+
+}
---
+++ gamejam-plus-2020-jogo/Assets/ProjectAssets/Scripts/Item/Bonfire.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bonfire : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other) {
+        if(other.tag.Equals("Player"))
+            GameLoopManager.instance.ReloadScene();
+    }
+}
