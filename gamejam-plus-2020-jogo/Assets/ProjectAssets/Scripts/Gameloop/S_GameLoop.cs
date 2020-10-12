f05c128071e625bf77b75c5f3637c1f26d00c1fb
felipe <felipe.higino.ps@gmail.com>
2020-10-11 21:35:32 -0300

    UI and gameloop

---
+++ gamejam-plus-2020-jogo/Assets/ProjectAssets/Scripts/Gameloop/S_GameLoop.cs
@@ -0,0 +1,20 @@
+﻿using FelipeUtils.Singleton;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class S_GameLoop : Singleton<S_GameLoop>
+{
+    //load or reload game
+    public void LoadGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void DestroyGame()
+    {
+        Destroy(Persistence.Instance.gameObject);
+        SceneManager.LoadScene(0);
+    }
+}
